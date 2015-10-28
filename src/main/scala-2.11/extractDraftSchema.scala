2b8bb7bb32889e0efc798b2d551759fa9622e9fb
Simon Shapiro <simon.m.shapiro@gmail.com>
2015-10-28 14:16:20 +0000

    New feature: extractDraftSchema

---
+++ src/main/scala-2.11/extractDraftSchema.scala
@@ -0,0 +1,44 @@
+import java.io.{FileWriter, BufferedWriter, File}
+import scala.collection.mutable.ListBuffer
+
+/**
+ * Created by simonshapiro on 27/10/15.
+ */
+
+
+object extractDraftSchema {
+  def main(args: Array[String]) {
+    if (args.length != 4) {
+      println("Usage <path to/csv file> <delimiter> <namespace> <path to/draft schema file>")
+    }
+    else {
+      val fName = args(0)
+      val separator = args(1).toCharArray
+      val namespace = args(2)
+      val draftFile = args(3)
+      val (status, labels, allLines) = CsvUtils.CsvReader(args(0), true, separator(0)).toBuffer
+      val listOfLabels = labels.split(separator).map(_.trim)
+      val schemaStrings = new ListBuffer[String]
+      schemaStrings += "{ \"namespace\": \"%s\",".format(namespace)
+      schemaStrings += "  \"name\":  \"%s\",".format(fName.split("/").last)
+      schemaStrings += "  \"type\":  \"record\","
+      schemaStrings += "  \"fields\": ["
+      val numberOfCols = listOfLabels.length
+      var count = 0
+      listOfLabels.foreach(label => {
+        count += 1
+        val avroCompatibleLabel = label.replaceAll("[' ']","_").replaceAll("[@|//?%-]","")
+        if (count == numberOfCols) schemaStrings += "          {\"name\": \"%s\", \"type\": \"string\"}".format(avroCompatibleLabel)
+        else schemaStrings += "          {\"name\": \"%s\", \"type\": \"string\"},".format(avroCompatibleLabel)
+      })
+      schemaStrings += "     ]"
+      schemaStrings += "}"
+      println(schemaStrings.mkString("\n"))
+      val file = new File(draftFile)
+      val bw = new BufferedWriter(new FileWriter(file))
+      bw.write(schemaStrings.mkString("\n"))
+      bw.close()
+
+    }
+  }
+}
